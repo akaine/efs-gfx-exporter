387fb3aa7d27bb91e3fd519ddc47fc122d3c1ac4
Boris Okolodkov <akaine.harga@gmail.com>
2019-04-28 18:17:14 -0500

    pcx writer test

--- src/test/java/TestPcx.java
+++ src/test/java/TestPcx.java
@@ -1,19 +1,27 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileOutputStream;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.formats.bmp.BmpImageParser;
 import org.apache.commons.imaging.formats.pcx.PcxImageParser;
+import org.apache.commons.imaging.formats.pcx.PcxWriter;
 
 public class TestPcx {
 
-    public static void main(final String[] args) throws IOException, ImageReadException {
+    public static void main(final String[] args) throws Exception {
 
         final PcxImageParser pcxImageParser = new PcxImageParser();
-        final BufferedImage pcxImage = pcxImageParser.getBufferedImage(new File("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\MANOWITZ\\BKIL01.PCX"), null);
+        final BufferedImage pcxImage = pcxImageParser.getBufferedImage(new File("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\PCX\\AIPLAT.PCX"), null);
 
-        ImageIO.write(pcxImage, "bmp", new File("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\MANOWITZ\\BKIL01.bmp"));
+        ImageIO.write(pcxImage, "bmp", new File("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\PCX\\AIPLAT.bmp"));
+
+        final BmpImageParser bmpImageParser = new BmpImageParser();
+        final BufferedImage bmpImage = bmpImageParser.getBufferedImage(new File("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\PCX\\AIPLAT.bmp"), null);
+
+        final PcxWriter pcxWriter = new PcxWriter(null);
+        pcxWriter.writeImage(bmpImage, new FileOutputStream("e:\\_CODE\\_PROJECTS\\efs_phoenix\\EFS\\PCX\\AIPLAT1.PCX"));
+
     }
 }
